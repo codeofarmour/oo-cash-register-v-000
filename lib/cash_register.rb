bd1f3396515f128a17e1a1b12fe0ed53ddf95569
Terri Riggle <codeofarmour@outlook.com>
2018-07-02 21:24:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,11 @@
 class CashRegister
-  attr_accessor :items, :
+  attr_accessor :items, :discount, :total, :last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+  
+  
 end
